08c5bc777801a4bd25685e26fa4657343cb19d4a
Lucas Chescheir <lucaschescheir@gmail.com>
2017-10-12 16:33:38 -0400

    changes

--- src/components/current-location.js
+++ src/components/current-location.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 
  function geoFindMe() {
   function success(position) {
-    var latitude  = position.coords.latitude;
+    let latitude  = position.coords.latitude;
     var longitude = position.coords.longitude;
 
 
-    var img = new Image();
+    let img = new Image();
     img.src = "https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyDWE4MPxffHrFCFgOJ62cm_0m1NRBwxm-g&center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
 
   }
@@ -22,7 +22,7 @@ geoFindMe()
 export default class CurrentLocation extends Component {
     render() {
         return(<div>
-            
+
         </div>
 
         )
