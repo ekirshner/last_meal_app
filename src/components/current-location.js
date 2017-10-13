c50c4526f2204f6cfc2e8028631677d46f4f057b
Lucas Chescheir <lucaschescheir@gmail.com>
2017-10-12 21:36:18 -0400

    cleaned up currentlocation

--- src/components/current-location.js
+++ src/components/current-location.js
@@ -1,37 +1,30 @@
 import React, {Component} from 'react';
 
-function geoFindMe() {
-    function success(position) {
-        let latitude = position.coords.latitude;
-        let longitude = position.coords.longitude;
-
-        let img = new Image();
-        img.src = "https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyDWE4MPxffHrFCFgOJ62cm_0m1NRBwxm-g&center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-
-    }
-
-    function error() {}
-
-    navigator.geolocation.getCurrentPosition(success, error);
-    return (
-        <div>img</div>
-    )
-}
-
 export default class CurrentLocation extends Component {
     constructor() {
         super()
         this.state = {
-            lat: 0,
-            long: 0
+            initialPosition: 0,
+            lastPosition: 0
         }
+    }
+
+    componentWillMount() {
+
+        function success(position) {
+
+            let latitude = position.coords.latitude;
+            let longitude = position.coords.longitude;
+            console.log(latitude, longitude)
+        }
+        function error() {
+            window.alert('location not found')
+        }
+        navigator.geolocation.getCurrentPosition(success, error);
     }
     render() {
         return (
-            <div>
-                {geoFindMe}
-            </div>
-
+            <div></div>
         )
     }
 }
