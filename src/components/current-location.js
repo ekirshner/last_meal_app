e7c37200430864654c33d90257991f51bbb98a00
Lucas Chescheir <lucaschescheir@gmail.com>
2017-10-18 09:37:10 -0400

    changed api fetch

--- src/components/current-location.js
+++ src/components/current-location.js
@@ -9,8 +9,8 @@ class CurrentLocation extends Component {
     }
 
     initMap() {
-            const lat = 35.225
-            const lng = -80.844
+            const lat = 37.7869
+            const lng =  -122.4041
 
     // Set initial map
             const map = new window.google.maps.Map(document.querySelector('#map'), {
@@ -40,7 +40,10 @@ class CurrentLocation extends Component {
 
     // Set the marker on the map for the user, add animation and set position of custom label
                 new window.google.maps.Marker({
-                    position: pos,
+                    position: {
+                        lat: 37.789,
+                        lng: -122.405
+                    },
                     map: map,
                     icon: '/blue_MarkerA.png',
                     animation: window.google.maps.Animation.DROP,
@@ -49,20 +52,30 @@ class CurrentLocation extends Component {
                 })
 
     // Recenter the map to the users location.
-                map.setCenter(pos)
+                map.setCenter(position: {
+                    lat: 37.789,
+                    lng: -121.405
+                })
+                fetch( 'https://warm-falls-44996.herokuapp.com/search/restaurants')
+                // fetch('https://itunes.apple.com/search?term=justin+bieber&limit=25')
+                    .then(res => res.json())
+                    .then(response => {
+                        console.log(response.businesses)
+                        // dispatch(getRestaurants(response.results));
+                    let resp = response.businesses
 
     // make fetch to the google places api and loop thru results.
-                const proxyurl = "https://cors-anywhere.herokuapp.com/";
-                console.log(pos.lat, pos.lng)
-                const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${pos.lat}, ${pos.lng}&radius=1000&type=food&key=AIzaSyABHYu_cSGt1eEdxuWyraZIJLVccqOX57E`
-                fetch(proxyurl + url).then(resp => resp.json()).then(resp => {
-                    console.log(resp.results)
+                // const proxyurl = "https://cors-anywhere.herokuapp.com/";
+                // console.log(pos.lat, pos.lng)
+                // const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${pos.lat}, ${pos.lng}&radius=1000&type=food&key=AIzaSyABHYu_cSGt1eEdxuWyraZIJLVccqOX57E`
+                // fetch(proxyurl + url).then(resp => resp.json()).then(resp => {
+                //     console.log(resp.results)
 
     // The code to set timeout for the icons dropping. Cant get it to work with
     // the infoWindow and addListener...
                     // let infoWindow;
                     // for (var i = 0; i < resp.results.length; i++) {
-                    //    addMarkerWithTimeout(resp.results[i].geometry.location, i * 200, resp.results[i].name);
+                    //    addMarkerWithTimeout(restaurants.loc, i * 200, resp.results[i].name);
                     //
                     //
                     //  }
@@ -81,26 +94,26 @@ class CurrentLocation extends Component {
                     //        }
     //loop thru api results.
 
-                    for (let i = 0; i < resp.results.length; i++) {
+                    for (let i = 0; i < resp.length; i++) {
                         let marker
                         let infoWindow
                         let markers= []
     // set markers for each result from api
                         marker = new window.google.maps.Marker({
                             position: { // coordinates from geocoding
-                                lat: resp.results[i].geometry.location.lat,
-                                lng: resp.results[i].geometry.location.lng
+                                lat: resp[i].coordinates.latitude,
+                                lng:resp[i].coordinates.longitude,
                             },
-                            title: resp.results[i].name,
+                            title: resp[i].name,
                             animation: window.google.maps.Animation.DROP,
                             map: map,  // map object we created in initMap
                             icon: '/paleblue_MarkerR.png ',
                         });
 
-    // set the window popup for each marker with info.
+    // // set the window popup for each marker with info.
                         infoWindow = new window.google.maps.InfoWindow({content: `
-                                <div><strong>${resp.results[i].name}</strong><br>
-                                Address: ${resp.results[i].vicinity}</div>
+                                <div><strong>${resp[i].name}</strong><br>
+                                Address: ${resp[i].location.address}</div>
                                 `})
 
     // add event listener to marker.
@@ -110,15 +123,18 @@ class CurrentLocation extends Component {
                         })
                     }
                 })
-            });
-        }
+                    });
+            };
+
 
     // init map AFTER component mounts.
         componentDidMount() {
             this.initMap();
+
         }
 
     render() {
+        console.log(this.props.restaurantList)
         return (
             <div className="App">
                 <div id="map">{this.marker}</div>
@@ -129,7 +145,7 @@ class CurrentLocation extends Component {
 
 function stateToProps(state) {
     return {
-        restaurant: state.restaurant,
+        restaurantList: state.restaurantList,
     };
 }
 
