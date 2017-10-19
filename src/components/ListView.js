d1ace09a237bd09fad9ea1197101734069ea3639
Erica Kirshner <ekkicks@gmail.com>
2017-10-19 10:00:01 -0400

    changes

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -11,15 +11,17 @@ class ListView extends Component {
         if(this.props.restaurants) {
         const restaurants = this.props.restaurants.map((restaurant, index) => {
             return <div key={ index } className="list-view-divs"> 
-                        <div>
-                            <img src={ restaurant.image_url } alt="" /> 
-                        </div>
+                        
                         <div>
                             <h3>{ restaurant.name }</h3>
-                            <p>{ restaurant.rating }</p>
-                            <p>{ restaurant.price }</p>
+                            <p></p>
+                            <p>{ restaurant.price }  { restaurant.rating }</p>
                             <p>{ restaurant.location.address1 }, { restaurant.location.city } { restaurant.location.zip_code }</p>
                             <Link to={ "/userDetails/" + index } ><button>Details</button></Link>
+                        </div>
+
+                        <div>
+                            <img src={ restaurant.image_url } alt="" /> 
                         </div>
                     </div>
         });
