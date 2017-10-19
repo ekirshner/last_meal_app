22d88b4982c595078df84cd75cfd4181c88fd456
Lucas Chescheir <lucaschescheir@gmail.com>
2017-10-19 14:36:00 -0400

    revert

--- src/App.js
+++ src/App.js
@@ -30,17 +30,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-        < img src={ logo } />
-         <h1> Last Meal, Great Deal! </h1>
-        </header>
+          <header>
+              < img src={ logo } />
+          </header>
 
         <main>
             <Switch>
               <Route path="/search" component={ Search } />
               <Route path="/userDetails/:id" component={ UserDetails } />
               <Route path="/paymentConfirmation" component={ PaymentConfirmation } />
-              <Route path="/payment" component={ Payment } /> 
+              <Route path="/payment" component={ Payment } />
 
               <Route path="/restaurantSignIn" component={ RestaurantSignIn } />
               <Route path="/restaurantDetails" component={ RestaurantDetails } />
