d13ec6f53ee439e3b5685ebd1266cedcc64da636
Lucas Chescheir <lucaschescheir@gmail.com>
2017-10-19 22:08:46 -0400

    fixed food facts

---
+++ src/facts.js
@@ -0,0 +1,15 @@
+let foodFacts=
+     [
+    '1.3 billion tons of food are wasted every year',
+'This amounts to £1 trillion of wasted or lost food',
+'If wasted food was a country, it would be the third largest producer of carbon dioxide in the world, after the United States and China',
+'Just one quarter of all wasted food could feed the 795 million undernourished people around the world who suffer from hunger',
+'Food waste in rich countries (222 million tons) is approximately equivalent to all of the food produced in Sub-Saharan Africa (230 million tons)',
+'A European or North American consumer wastes almost 100 kilograms of food annually, which is more than his or her weight (70 kilograms)',
+'A European or North American consumer wastes 15 times more food than a typical African consumer',
+'Lack of technology and infrastructure is the main cause of food waste in Africa, as opposed to household food waste in the developed world',
+'Food waste in Europe alone could feed 200 million hungry people',
+'Food waste generates 3.3 billions tons of carbon dioxide, which accelerates global climate change'
+]
+
+export {foodFacts};
