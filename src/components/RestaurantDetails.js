44cf382f83afb9ff56559d6794d0f9044495f1aa
Erica Kirshner <ekkicks@gmail.com>
2017-10-25 15:49:48 -0400

    tweaking

--- src/components/RestaurantDetails.js
+++ src/components/RestaurantDetails.js
@@ -19,19 +19,20 @@ class RestaurantDetails extends Component {
 
         };
     }
-// When a user clicks delete, send a DELETE request to backend
+
+    // When a user clicks delete, send a DELETE request to backend
     onDeleteClick(index) {
          console.log('deleted!')
-         const url= 'https://warm-falls-44996.herokuapp.com/inventory/'+index
+         const url= 'https://warm-falls-44996.herokuapp.com/inventory/'+ index
          fetch(url , {
              method: 'DELETE',
              credentials: 'include',
              headers: {
              'Content-Type': 'application/json',
-         },
-         })
+            },
+        })
 
-}
+    }
 
     // When a user clicks the add item button, display the form
     toggleVisibility () {
@@ -46,87 +47,119 @@ class RestaurantDetails extends Component {
             setPickupTimeVisible: !this.state.setPickupTimeVisible,
         });
     }
-handleChange(event) {
-    console.log(event.target.value)
-    this.setState({
-        pickup_time: event.target.value + ":00",
-    })
-}
-handleChange2(event) {
-    this.setState({
-        pickup2_time: event.target.value + ":00",
-    })
-}
-handleClick() {
-    console.log(this.state.pickup_time, this.state.pickup2_time)
-}
-handleItemClick(){
-    console.log(this.props.restaurant)
-    console.log(this.state.description, this.state.price, this.state.num_available)
-    console.log(this.state.pickup_time, this.state.pickup2_time)
-    fetch('https://warm-falls-44996.herokuapp.com/inventory', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-        'Content-Type': 'application/json',
-    },
-//mode: 'no-cors',
-  body: JSON.stringify({
+        
+    handleChange(event) {
+        // console.log(event.target.value)
+        this.setState({
+            pickup_time: event.target.value + ":00",
+        })
+    }
+
+    handleChange2(event) {
+        this.setState({
+            pickup2_time: event.target.value + ":00",
+        })
+    }
+
+    // Edit the Pickup Times
+    handleClick() {
+        console.log(this.state.pickup_time, this.state.pickup2_time)
+        fetch('https://warm-falls-44996.herokuapp.com/inventory', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify ({
+                description: this.props.restaurant.inventory[1].description,
+                num_available: this.props.restaurant.inventory[1].num_available,
+                pickup_start: this.state.pickup_time,
+                pickup_end: this.state.pickup2_time,
+                price: this.props.restaurant.inventory[1].price,
+            })
+        })
+    }
+
+    // Add Menu Item
+    handleItemClick() {
+        // console.log(this.props.restaurant)
+        // console.log(this.state.description, this.state.price, this.state.num_available)
+        // console.log(this.state.pickup_time, this.state.pickup2_time)
+        fetch('https://warm-falls-44996.herokuapp.com/inventory', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify ({
                 description: this.state.description,
                 num_available: parseInt(this.state.num_available),
                 pickup_start: this.state.pickup_time,
                 pickup_end: this.state.pickup2_time,
                 price: parseInt(this.state.price),
-
             })
-})
-}
-handlePriceChange(event) {
-    this.setState({
-        price: event.target.value,
-    })
-}
-handleNumAvailable(event) {
-    this.setState({
-        num_available: event.target.value,
-    })
-}
-handleItemChange(event) {
-    this.setState({
-        description: event.target.value,
-    })
-}
+        })
+    }
+
+    handlePriceChange(event) {
+        this.setState({
+            price: event.target.value,
+        })
+    }
+
+    handleNumAvailable(event) {
+        this.setState({
+            num_available: event.target.value,
+        })
+    }
+
+    handleItemChange(event) {
+        this.setState({
+            description: event.target.value,
+        })
+    }
 
     render() {
         if (!this.props.restaurant.inventory) {
                 return <div />;
         }
 
-        console.log(this.props.restaurant.inventory[0])
+        const items = this.props.restaurant.inventory.map((item, index)=> {
+            // console.log(item.price)
+            if(item.num_available > 0) {
+                return(
+                    <tr key={ index }>
+                        <td onClick={ () => this.onDeleteClick(item.id) }><img className="delete" src={ plus }/></td>
+                        <td>${ item.price }</td>
+                        <td>{ item.description }</td>
+                        <td>{ item.num_available } remaining</td>
+                    </tr>)
+                }
+        })
 
-const items = this.props.restaurant.inventory.map((item, index)=> {
-    console.log(item.price)
-    if(item.num_available > 0) {
-    return(
-    <tr key={index}>
-        <td onClick={ () => this.onDeleteClick(item.id) }><img className="delete" src={ plus }/></td>
-        <td>${item.price}</td>
-        <td>{item.description}</td>
-        <td>{item.num_available}</td>
-    </tr>)
-}
-})
+        // Convert military time to standard time
+        function tConvert(timeString) {
+            let hourEnd = timeString.indexOf(":");
+            let H = +timeString.substr(0, hourEnd);
+            let h = H % 12 || 12;
+            let ampm = (H < 12 || H === 24) ? "AM" : "PM";
+            timeString = h + timeString.substr(hourEnd, 3) + ampm;
+
+            return timeString;
+        }
+
+
         return (
             <div className="restaurant-details-component">
-                <p className="welcome">Welcome, {this.props.restaurant.name}!</p>
-                <h2>{this.props.restaurant.name}</h2>
-                <p className="location">{this.props.restaurant.display_address}</p>
+                <p className="welcome">Welcome, { this.props.restaurant.name }!</p>
+                <h2>{ this.props.restaurant.name }</h2>
+                <p className="location">{ this.props.restaurant.display_address }</p>
 
                 <Row className="show-grid">
                     <Col xs={12} md={6} lg={6}>
                         <div>
                             <h3>Set Pick Up Time</h3>
-                            <p>(dynamically populate pickup times)</p>
+                            <p>{ tConvert(this.props.restaurant.inventory[1].pickup_start) } - { tConvert(this.props.restaurant.inventory[1].pickup_end) } </p>
                             <div className="add-item">
                                 <img src={ plus } onClick={ () => this.toggleVisibilityPickup() } />
                                 <p>Change Pick Up Time</p>
@@ -160,7 +193,7 @@ const items = this.props.restaurant.inventory.map((item, index)=> {
                                 <div className="add-item-dropdown">
                                     <label>Menu Item</label>
                                     <input type="text" placeholder="Item Name"
-                                        value= {this.state.description}
+                                        value= { this.state.description }
                                         onChange={ (ev)=>this.handleItemChange(ev) }/>
                                     <label>Item Price  $</label>
                                     <input type="number" min="1" max="10" placeholder="5"
@@ -182,7 +215,7 @@ const items = this.props.restaurant.inventory.map((item, index)=> {
                             <h3> Current Menu Items </h3>
                             <table>
                                 <tbody>
-                                    {items}
+                                    { items }
                                 </tbody>
                             </table>
                         </section>
@@ -195,7 +228,9 @@ const items = this.props.restaurant.inventory.map((item, index)=> {
 
 function MapState2Props(state) {
     return {
-    restaurant: state.restaurant,
+        restaurant: state.restaurant,
+    }
 }
-}
+
+
 export default connect(MapState2Props, null)(RestaurantDetails);
