3e356f0f16e3c97209fd5cefadee6c41799d8c9e
Lucas Chescheir <lucaschescheir@gmail.com>
2017-10-26 13:53:52 -0400

    changed mapview icons

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
-// import {foodFacts} from '../facts'
-import FoodFacts from './foodFacts';
+import {foodFacts} from '../facts'
 
 class SignIn extends Component {
 // change
@@ -13,31 +12,30 @@ class SignIn extends Component {
     //             }
     //     return <button onClick={() => this.props.authenticate(false)} type="submit">Sign Out</button>
     // }
-
     handleSubmit = () => {
          this.props.history.push("/search");
     }
-    
-    // renderFacts() {
-    //     return (
-    //         foodFacts[Math.floor(Math.random() * 9)]
-    //     )
-    // }
+
+    renderFacts() {
+    return (
+    foodFacts[Math.floor(Math.random() * 10)]
+)
+     }
 
     render() {
+        // console.log(foodFacts)
         return (
             <div className="sign-in-view">
-                {/* <p>{ this.renderFacts() }</p> */}
-                 <p><FoodFacts /></p>
+                <p>{this.renderFacts()}</p>
                 <div id="sign-in">
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
-                    <button onClick={ () => this.handleSubmit() } type="submit">Sign In</button>
+                    <input type="email" placeholder="Email" required="required"/>
+                    <input type="password" placeholder="Password" required="required"/>
+                    <button onClick={() => this.handleSubmit()} type="submit">Sign In</button>
                     <h5><a href="">Forgot Password?</a></h5>
                 </div>
 
                 <div className="signiin-footer">
-                    <h5>Don't have an account? Register
+                    <h5>Are you a restuarant owner? Click
                         <a href="/RestaurantSignIn"> here!</a>
                     </h5>
                 </div>
@@ -45,7 +43,6 @@ class SignIn extends Component {
         );
     }
 }
-
 
 function MapState2Props(state) {
     return {authenticated: state.authenticated}
